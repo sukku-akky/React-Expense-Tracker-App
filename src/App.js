9e342082fcc345b33ac489d8a428ff5cc74eefb0
sukku-akky <somasukku@gmail.com>
2024-10-21 13:01:57 +0530

    redux-commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-import React,{useContext, useEffect} from "react";
-import Profile from "./components/Profile";
-import SignUpForm from "./components/SignUpForm";
-import MyNavbar from "./components/MyNavbar";
 import { BrowserRouter as Router, Route, Routes ,useLocation,Navigate}from 'react-router-dom';
 import HomePage from "./pages/HomePage";
-import { useDispatch,useSelector } from "react-redux";
-import { authActions } from "./store/auth-redux";
+import SignUpForm from './components/SignUpForm';
 import ForgotPassword from "./pages/ForgotPassword";
 import ProfilePage from "./pages/ProfilePage";
-import { fetchDataFromEnd } from "./store/expense-actions";
-import userEvent from "@testing-library/user-event";
+import { useSelector,useDispatch } from 'react-redux';
+import { authActions } from './store/auth-redux';
+import MyNavbar from './components/MyNavbar';
 function App(){
-  const dispatch=useDispatch();
-  const isLoggedIn=useSelector(state=>state.auth.isLoggedIn);
-  useEffect(()=>{
-    dispatch(fetchDataFromEnd());
-
-  },[])
- 
+  const isLoggedIn=useSelector(state=>state.auth.isLoggedIn)
   return (
-    <>
+  <>
     <Router>
       <MyNavbar/>
-      
       <Routes>
         <Route path="/" element={isLoggedIn ? <HomePage/>: <Navigate to="/auth"/>}/>
         <Route path="/auth" element={<SignUpForm/>}/>
         <Route path="/profile" element={isLoggedIn ? <ProfilePage/> :<Navigate to="/auth"/>}/>
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
-      
+
 
     </Router>
 
